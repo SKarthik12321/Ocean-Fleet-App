90cf9fb4a49d716e9d2cacf12fcbd4af4d26bd0d
S KARTHIK <skarthik.link@gmail.com>
2026-02-03 15:55:19 +0530

    [S Karthik] UC3: get vessel by id

--- src/VesselUtil.java
+++ src/VesselUtil.java
@@ -16,4 +16,14 @@ public class VesselUtil {
     public void addVesselPerformance(Vessel vessel) {
         vesselList.add(vessel);
     }
+
+    // UC3
+    public Vessel getVesselById(String vesselId) {
+        for (Vessel v : vesselList) {
+            if (v.getVesselId().equals(vesselId)) {
+                return v;
+            }
+        }
+        return null;
+    }
 }
